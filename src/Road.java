86ef2f463f8cc0132b3b70d5f3243a0c4c096a03
princeoflight <nsmichael31@gmail.com>
2015-11-07 09:28:22 -0600

    added set methods to road class

--- src/Road.java
+++ src/Road.java
@@ -2,15 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Road {
-	public int num_cars;
-	public int speed_limit;
-	public int road_length = 3000;
-	public int num_lanes = 2;
-	public int roadblock_position = 2500;
+	private int num_cars; // number of cars
+	private int speed_limit; // speed limit of road
+	private int road_length = 3000; // road length - in meters
+	private int num_lanes = 2; // number of lanes
+	private int roadblock_position = 2500; // position of road-block from beginning of road
+	
+	private int num_sedans;
+	private int num
 	
 	// lanes
 	// each row is a pair of lanes
-	List<List<Vehicle>> road = new ArrayList<List<Vehicle>>();
+	private List<List<Vehicle>> road = new ArrayList<List<Vehicle>>();
 	
 	public Road(){
 		// 4 | |X|
@@ -19,21 +22,39 @@ public class Road {
 		// 1 | | |
 		// 0 |X| |
 		
-		for(int i=0;i<road_length;i++){ 
-			for(int j=0;j<num_lanes;j++){
-				
-			}
+		// sets position of cars
+		for(int i=0;i<num_cars;i++){
+			Vehicle v = new Sedan();
+			
+			
 		}
 	}
 	
+	public Road(ArrayList<Vehicle>){
+		
+	}
+	
+	// sets number of cars
+	public void setNumCars(int n){
+		this.num_cars = n;
+	}
+	
+	// sets length of road
 	public void setRoadLength(int n){
 		this.road_length = n;
 	}
 	
+	// set number of lanes
+	public void setNumLanes(int n){
+		this.num_lanes = n;
+	}
+	
+	// set position of road block
 	public void setRoadBlock(int n){
 		this.roadblock_position = n;
 	}
 	
+	// increments through each second
 	public void nextSecond(){
 		
 	}
