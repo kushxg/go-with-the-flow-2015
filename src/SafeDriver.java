7699a45e79cb8192cdac8598868e6ec8998be447
gupta1000 <kush.kg.gupta@gmail.com>
2015-11-07 09:36:14 -0600

    added driverlogic classes and edited vehicle

--- src/SafeDriver.java
+++ src/SafeDriver.java
@@ -1,16 +1,8 @@
 
-public class DriverLogic {
-	
-	public int anger;
-	public String type;
-	
-	public DriverLogic (){
-		
-	}
-	
+public class SafeDriver extends DriverLogic {
+
 	public boolean drive(Road road, Vehicle v) {
 		
 		return false;	// false implies that a crash did not take place
 	}
-	
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -2,16 +2,16 @@ import java.util.List;
 
 
 public class Vehicle {
-	private int max_speed;			// the maximum speed attainable by this kind of car
-	private int speed;				// the current speed of this car
-	private int length;				// the length of a car
-	private DriverLogic logic;
+	public int max_speed;			// the maximum speed attainable by this kind of car
+	public int speed;				// the current speed of this car
+	public int length;				// the length of a car
+	public DriverLogic logic;
 	
 	public Vehicle() {
 		
 	}
 	
-	public boolean drive(Road road, Vehicle this) {
-		return logic.drive(road); 
+	public boolean drive(Road road) {
+		return logic.drive(road, this);
 	}
 }
