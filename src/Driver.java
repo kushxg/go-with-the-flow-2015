b32b9a3ea0e8ee151da4046d1b16c40ab97f6ff0
princeoflight <nsmichael31@gmail.com>
2015-11-07 08:42:30 -0600

    starting from scratch

--- src/Driver.java
+++ src/Driver.java
@@ -2,20 +2,7 @@
 public class Driver {
 
 	public static void main(String[] args) {
-		printHello();
+		
 	}
 
-	
-	private static void printHello() {
-		System.out.println("hello");
-	}
-	
-	private void noor() {
-		System.out.println("im noor now");
-	}
-	
-	private void kush() {
-		
-		
-	}
 }
