ceefd55e2e6143586c6813264bcf4e4a41adab6a
princeoflight <nsmichael31@gmail.com>
2015-11-07 18:27:48 -0600

    completed output

--- src/Driver.java
+++ src/Driver.java
@@ -1,14 +1,18 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class Driver {
 	private static PrintWriter file; // for sets of tests
-	private static PrintWriter masterFile; // for overall analysis of data
+	private static FileWriter masterFile; // for overall analysis of data
 	private static BufferedReader reader;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// each set of tests gets printed out in file
 		
 		String text = "";
@@ -41,10 +45,10 @@ public class Driver {
 			file = new PrintWriter(temp);
 		} catch (Exception e) {System.out.println("file writing error");}
 		try {
-			masterFile = new PrintWriter("master_file.csv");
+			masterFile = new FileWriter("master_file.csv", true);
 		} catch (Exception e) {System.out.println("master file write error");}
 		
-		file.println("# CARS,# SEDANS,# VANS,# TRUCKS,# RANDOM DRIVERS,# AGGRESSIVE DRIVERS,# CONSERVATIVE DRIVERS,# SLOW DRIVERS,# FAST DRIVERS,# SAFE DRIVERS,# SELFISH DRIVERS,# FAIR DRIVERS,# TEST DRIVERS,TOTAL RUNTIME,AVG. TIME/CAR,CRASHED");
+		// masterFile.append("TEST FILE,# CARS,# SEDANS,# VANS,# TRUCKS,# RANDOM DRIVERS,# AGGRESSIVE DRIVERS,# CONSERVATIVE DRIVERS,# SLOW DRIVERS,# FAST DRIVERS,# SAFE DRIVERS,# SELFISH DRIVERS,# FAIR DRIVERS,# TEST DRIVERS,AVG. TOTAL RUNTIME,AVG. AVG. TIME/CAR,# CRASHES\n");
 		
 		/* SET UP TESTS */
 		int num_sedans = 0, num_vans = 0, num_trucks = 0;
@@ -53,7 +57,11 @@ public class Driver {
 		int total_runtime = 0;
 		double avg_time_per_car = 0;
 		
-		// first test
+		// beginning of test
+		file.println("TOTAL RUNTIME,AVG TIME PER CAR,CRASHED");
+		
+		int num_tests = 5; // number of tests
+		
 		Road r = new Road();
 		
 		num_sedans = 50;
@@ -61,6 +69,11 @@ public class Driver {
 		
 		num_random_drivers = 50;
 		
+		double avg_runtime = 0;
+		double avg_avg_time_per_car = 0;
+		int num_crashes = 0;
+		
+		// loop over num_tests ############################ add loop into code
 		int pos;
 		int lane;
 		for(int i=0;i<num_sedans;i++){
@@ -75,11 +88,21 @@ public class Driver {
 		r.run();
 		
 		total_runtime = r.total_runtime;
+		avg_runtime += total_runtime;
 		avg_time_per_car = r.avg_time_per_car;
+		avg_avg_time_per_car += avg_time_per_car;
 		
 		String crash_result = (r.crashed) ? "X" : ""; // TYPE OF DRIVER THAT CRASHES
+		if(r.crashed) num_crashes++;
 		
-		file.println(num_cars + "," + num_sedans + "," + num_vans + "," + num_trucks + "," + num_random_drivers + "," + num_aggressive_drivers + "," + num_conservative_drivers + "," + num_slow_drivers + "," + num_fast_drivers + "," + num_safe_drivers + "," + num_selfish_drivers + "," + num_fair_drivers + "," + num_test_drivers + "," + total_runtime + "," + avg_time_per_car + "," + crash_result);
+		file.println(total_runtime + "," + avg_time_per_car + "," + crash_result);
+		
+		// end loop
+		
+		avg_runtime = ((double) avg_runtime) / num_tests;
+		avg_avg_time_per_car /= num_tests;
+		
+		masterFile.append(temp + "," + num_cars + "," + num_sedans + "," + num_vans + "," + num_trucks + "," + num_random_drivers + "," + num_aggressive_drivers + "," + num_conservative_drivers + "," + num_slow_drivers + "," + num_fast_drivers + "," + num_safe_drivers + "," + num_selfish_drivers + "," + num_fair_drivers + "," + num_test_drivers + "," + avg_runtime + "," + avg_avg_time_per_car + "," + num_crashes + "\n");
 		
 		// end of test
 		
